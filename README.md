e47c137e17148c489f1402c2cca21ac0c7f5d559
hhwa782 <93207559+hhwa782@users.noreply.github.com>
2021-11-01 23:32:50 +0900

    홈페이지 기본 레이아웃 생성
    Header, Footer 컴포넌트 추가

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VuePractice
 Vue project for practice
+
+vuejs 게시판 만들기
+참고: https://fyzh99.tistory.com/
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Header />
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/common/Header.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Footer from './components/common/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
+    , HelloWorld
+    , Footer
   }
 }
 </script>
 
 <style>
+html,body{padding:0; margin:0;}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin:0; padding:0;
 }
 </style>
